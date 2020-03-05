56668bf45db20fd6445aca6c00ae835f9c10fe8c
Tomek Paczkowski <tomek@hauru.eu>
2020-03-05 22:56:56 +0000

    Add Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :counter, CounterWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "silly-counter.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
-
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
--- lib/counter_web/endpoint.ex
+++ lib/counter_web/endpoint.ex
@@ -10,7 +10,8 @@ defmodule CounterWeb.Endpoint do
     signing_salt: "mAurPLQ3"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000, connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
